0d75e3d1d3a6ccfc4ee490fcd29e0a5807b68764
Ashmit Prajapati <ashmitgithub@gmail.com>
2023-02-03 22:09:34 +0530

    03.02.2023

---
+++ CodeWithHarry (C)/11_forloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 10;
+    int i;
+    for(i = 0; i < num; i++) { 
+    printf("%d ",i);
+}
+    /*int i1, j=0;
+    for(i1=0 ;  ; )
+    {
+       printf("%d %d\n", i1, j);
+       i1++;j++;
+    }*/
+    return 0;
+}
