81c3020d44150859bbca9414799278cf4b9aac77
Ashmit Prajapati <ashmitgithub@gmail.com>
2023-02-08 23:14:18 +0530

    08.02.2023

--- CodeWithHarry (C)/1_main.c
+++ CodeWithHarry (C)/1_main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     printf("Hello world\n");
---
+++ CodeWithHarry (C)/23_StarPattern.c
@@ -0,0 +1,23 @@
+/* Take input from the user and ask user to choose 0 for triangular star pattern and 1 for reversed triangular star pattern
+Then print the pattern accordingly
+*
+**
+***
+**** -> Triangular star pattern
+
+
+*****
+****
+***
+**
+* -> Reversed triangular star pattern
+
+*/
+#include <stdio.h>
+int main()
+{
+    
+    return 0;
+}
+
+
